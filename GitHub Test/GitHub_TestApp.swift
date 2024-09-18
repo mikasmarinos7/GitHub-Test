4186686a702bc18d7c3e67d30edfe2f169423bee
mikasmarinos7 <mikasmarinos@icloud.com>
2024-09-18 14:39:12 -0400

    Initial Commit

---
+++ GitHub Test/GitHub_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_TestApp.swift
+//  GitHub Test
+//
+//  Created by Mikas Marinos on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
